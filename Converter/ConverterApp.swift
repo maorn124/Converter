83848ab538e8266929cdf6c68f859eb685a4f15e
maorn124 <maor.niv@gmail.com>
2024-06-02 10:51:33 -0400

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Maor Niv on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
